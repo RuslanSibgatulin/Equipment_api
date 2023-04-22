5adef6ec7aa1c2b9ee875241f56cd4184d018038
Ruslan Sibgatulin <doolf@yandex.ru>
2023-04-22 16:02:53 +0300

    Fix dict typing

--- app/config/exc_handler.py
+++ app/config/exc_handler.py
@@ -1,11 +1,11 @@
 from http import HTTPStatus
-from typing import Any
+from typing import Any, Dict
 
 from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework.views import Response, exception_handler
 
 
-def api_exception_handler(exc: Exception, context: dict[str, Any]) -> Response:
+def api_exception_handler(exc: Exception, context: Dict[str, Any]) -> Response:
     """Custom API exception handler."""
 
     response = exception_handler(exc, context)
